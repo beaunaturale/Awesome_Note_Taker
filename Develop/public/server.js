74a9de042383766f0241ccab17e8cbcdf6951707
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-05-10 08:04:12 -0500

    added middlewrae to server.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,40 +1,41 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const PORT = 3001;
+const fs = require('fs');
 
+const app = express();
+
+const notes = require('/db/db.json')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/api/notes', (req, res) => {
+  res.json(notes.slice(1));
+});
 
-app.get('send', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
-
-app.get('/routes', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/routes.html'))
-);
-
-app.listen(PORT, () =>
-console.log('Example app listening at http://localhost:${PORT}')
-);
-
-// We can send a body parameter to the client using the res.send() method. This body parameter can be a string, buffer, or even an array.
 app.get('/', (req, res) => {
-  res.send(
-    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
-  );
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// This method sends a JSON response with the correct content type.
-app.get('/api', (req, res) => {
-  res.json({
-    term: 'api',
-    description:
-      'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
-  });
+app.get('/notes', (req, res) => {
+res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-app.listen(PORT, () =>
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+// note function
+// delete function
+
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(req.params.id, notes)
+  res.json(true);
+});
+
+
+app.listen(PORT, () => 
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
